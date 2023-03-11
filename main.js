5e1a2f866499648f50638d6ff0fd5c5b5796d722
kerberosilicious <123393126+kerberosilicious@users.noreply.github.com>
2023-03-12 00:09:54 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 onload = () =>{
     document.body.classList.remove("container");
-};
+
+var play = true;
+
+var myAudio = document.getElementById("leson");
+
+function onKeyDown(event) {
+        switch (event.keyCode) {
+            case 32: //SpaceBar                    
+                if (play) {
+                    myAudio.pause();
+                    play = false;
+                } else {
+                    myAudio.play();
+                    play = true;
+                }
+                break;
+        }
+  return false;
+}
+
+window.addEventListener("keydown", onKeyDown, false);
+}; 
