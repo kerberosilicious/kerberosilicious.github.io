d5d7d5d72a178a2389147ae6829ab5c587dd6a02
kerberosilicious <123393126+kerberosilicious@users.noreply.github.com>
2023-03-12 09:12:28 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 onload = () =>{
     document.body.classList.remove("container");
 
-var source = "Radiohead - No Surprises.mp3"
-var audio = document.createElement("audio");
-//
-audio.autoplay = true;
-//
-audio.load()
-audio.addEventListener("load", function() { 
-    audio.play(); 
-}, true);
-audio.src = source;
+    var source = "Radiohead - No Surprises.mp3"
+    var audio = document.createElement("audio");
+    //
+    audio.autoplay = true;
+    //
+    audio.load()
+    audio.addEventListener("load", function() { 
+        audio.play(); 
+    }, true);
+    audio.src = source;
 }; 
