44dfda9ad3aece29cc5c001d07197321af8145f4
kerberosilicious <123393126+kerberosilicious@users.noreply.github.com>
2023-03-12 09:21:11 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 onload = () =>{
     document.body.classList.remove("container");
 
-    var source = "Radiohead - No Surprises.mp3"
-    var audio = document.createElement("audio");
-    //
-    audio.autoplay = true;
-    //
-    audio.load()
-    audio.addEventListener("load", function() { 
-        audio.play(); 
-    }, true);
-    audio.src = source;
+    audioElement.play();
 }; 
